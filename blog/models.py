3bd8b9e9d7bcc450661bae29262736cae4648b9b
Matt <mattppalmer@gmail.com>
2019-08-01 17:59:40 -0400

    blogs polishing

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pubdate = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True, default='')
