3fc8201087384aa9c77f39e22eb338291a5ed625
Matt <mattppalmer@gmail.com>
2019-08-02 08:05:12 -0400

    blog details

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,14 @@ class Blog(models.Model):
     pubdate = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/', blank=True, default='')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_formatted(self):
+        return self.pubdate.strftime('%b. %e, %Y')
+
+    
