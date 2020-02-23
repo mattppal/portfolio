f3ccca01c7a7736c40275b8d18607c50b30662e1
matt <matt.palmer@storyblocks.com>
2020-02-23 13:18:17 -0500

    add in job ordering

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,4 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.TextField(max_length=800, blank=True)
     link = models.URLField(blank=True)
+    num = models.IntegerField(blank=True)
